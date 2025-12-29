4aa63d4088fcf70fd61894cd7dec2cf5affa9a63
RayZ3R0 <iamrayyanahsan@gmail.com>
2025-12-30 00:42:52 +0600

    fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,10 @@
 
 fn main() {
     #[cfg(target_os = "linux")]
-    std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+    {
+        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+        std::env::set_var("GDK_BACKEND", "x11");
+    }
 
     sonami_lib::run()
 }
